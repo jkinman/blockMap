6dcd56ffbdb9b2c2f3ebd2e8c2e91e8aac6a7c44
jkinman <joel.kinman@gmail.com>
2018-03-02 09:02:37 -0800

    update dist

--- src/components/App.js
+++ src/components/App.js
@@ -195,7 +195,8 @@ class AppComponent extends React.Component {
     return (
       <div className="index">
         <h1>Etherium Blockchain Bitmap Visualization</h1>
-        <h3>The transactions in every block are being converted to RGB values and rendered to a bitmap image</h3>
+        <h3>The transactions in every block are being contatenated and converted to RGB values and the result is being rendered to a bitmap.</h3>
+        <h3>Click the blocks to inspect</h3>
         <p ref="data"></p>
         <div className="blockContainer">
           { this.state.blockArray && 
